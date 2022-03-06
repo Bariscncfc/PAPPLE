0c485be09e52c5efad41ae806de6a010bb3371ad
Barış Can Çifçi <78167969+Bariscncfc@users.noreply.github.com>
2022-03-06 13:11:53 +0300

    New Arrival Component is created and designed. Component data is pulled data.js file

--- papple/src/App.js
+++ papple/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Header from "./components/Header";
 import Hero from './components/Hero';
+import Arrival from './components/Arrival';
 
 function App() {
   return (
     <div>
       <Header/>
         <Hero/>
+        <Arrival/>
     </div>
   );
 }
